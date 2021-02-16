bbd5874c7689ffa32031fcace30c688924f9c4c1
Waqas-janii <waqas5682@gmail.com>
2021-02-16 21:41:23 +0500

    update

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ DB_HOST = "mongodb+srv://ai_rn:TYwqhq9vVZ8qKh1G@cluster0-elnjz.mongodb.net/v-lum
 DB_PORT = ""
 DB_USER = "ai_rn"
 DB_PASS = "TYwqhq9vVZ8qKh1G"
-
+ 
 connection = MongoClient(DB_HOST)
 # db = connection.resumas
 db = connection[DB_NAME]
